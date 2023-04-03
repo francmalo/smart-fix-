cfdb1f2018e9497eeeafe19232752f10319cfd88
francis malombe <109952642+francmalo@users.noreply.github.com>
2023-04-03 12:36:00 +0300

    second commit

--- config/settings.py
+++ config/settings.py
@@ -78,14 +78,22 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.sqlite3',
+   #     'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'test',
+        'USER':'postgres',
+        'PASSWORD':'safaricom',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
